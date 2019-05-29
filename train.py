babe5b1e7c92685e9fe8c681f992c7a803137060
xufuyong <vector_xu@163.com>
2019-05-29 20:23:44 +0800

    finish __init__,train

--- train.py
+++ train.py
@@ -74,3 +74,109 @@ def train(train_loader,net,criterion,optimizer,epoch,device):
     writer.add_scalar('train_acc',train_acc,epoch)
 
     return train_loss,train_acc
+
+#val
+def test(test_loader, net, criterion, optimizer, epoch, device):
+    global best_prec, writer
+
+    net.eval()
+
+    test_loss = 0
+    correct = 0
+    total = 0
+
+    logger.info("===== Validate =====".format(epoch+1,config.epochs))
+
+    with torch.no_grad():
+        for batch_index, (inputs, targets) in enumerate(test_loader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _,predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+    logger.info("  ===test loss:{:.3f}|test acc{:6.3f}%".format(test_loss/(batch_index + 1), 100.0*correct/total))
+    test_loss = test_loss / (batch_index + 1)
+    test_acc = correct / total
+    writer.add_scalar('test_loss', test_loss, epoch)
+    writer.add_scalar('test_acc', test_acc, epoch)
+
+    #Save checkpoint
+    acc = 100.*correct/total
+    state = {
+        'state_dict': net.state_dict(),
+        'best_prec': best_prec,
+        'last_epoch': epoch,
+        'optimizer': optimizer.state_dict(),
+    }
+    is_best = acc > best_prec
+    save_checkpoint(state, is_best, args.work_path + '/' + config.ckpt_name)
+    if is_best:
+        best_prec = acc
+
+
+def main():
+    global args, config, last_epoch, best_prec, writer
+    writer = SummaryWriter(logdir=args.work_path+'/event')
+
+    #read config from yaml file
+    with open(args.work_path + '/config.yaml') as f:
+        config = yaml.load(f)
+
+    #convert to dict
+    config = EasyDict(config)           #easydict的作用：可以使得以属性的方式去访问字典的值
+    logger.info(config)
+
+    #denfine net
+    net = get_model(config)
+    logger.info(net)
+    logger.info("===total parameters:" + str(count_parameters(net)))
+
+    #GPU or CPU
+    device = 'cuda' if config.use_gpu else 'cpu'
+    #data parallel for multiple-GPU
+    if device == 'cuda':
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = True
+    net.to(device)
+
+    #define loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(net.parameters(),config.lr_scheduler.base_lr,
+                                momentum=config.optimize.momentum,
+                                weight_decay=config.optimize.weight_decay,
+                                nesterov=config.optimize.nesterov)
+
+    #resume from a checkpoint
+    last_epoch = -1
+    best_prec = 0
+    if args.work_path:
+        ckpt_file_name = args.work_path + '/' + config.ckpt_name + '.pth.tar'
+        if args.resume:
+            best_prec,last_epoch = load_checkpoint(ckpt_file_name,net,optimizer)
+
+    #load training data,do data augmentation and get data loader
+    transform_train = transforms.Compose(
+        data_augmentation(config)
+    )
+    transform_test = transforms.Compose(
+        data_augmentation(config, is_train=False)
+    )
+
+    train_loader, test_loader = get_data_loader(transform_train,transform_test,config)
+
+    #start training
+    logger.info("        =======  start  training   ======     ")
+    for epoch in range(last_epoch+1,config.epochs):
+        lr = adjust_learning_rate(optimizer, epoch, config)
+        writer.add_scalar('learning_rate', lr, epoch)
+        train(train_loader, net, criterion, optimizer, epoch, device)
+        if epoch == 0 or (epoch + 1) % config.eval_freq == 0 or epoch == config.epochs - 1:
+            test(test_loader, net, criterion, optimizer, epoch, device)
+    logger.info("=====Training Finished.   best_test_acc:{:.3f}%====".format(best_prec))
+
+if __name__ == "__main__":
+    main()
